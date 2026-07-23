712839bc122f2cb82dddff508eed54c70f91456d
fabri236 <fabrizio_m@hotmail.com>
2026-07-23 17:41:00 -0300

    Add missing challenge to transcript pattern

--- sumcheck/src/sumcheck2/folding/reduction.rs
+++ sumcheck/src/sumcheck2/folding/reduction.rs
@@ -49,7 +49,9 @@ where
         builder: TranscriptBuilder,
     ) -> TranscriptBuilder {
         let degree = key.degree + 1;
-        builder.round::<F, SumcheckMessage<F>, 1>(&degree)
+        builder
+            .round::<F, (), 1>(&())
+            .round::<F, SumcheckMessage<F>, 1>(&degree)
     }
 
     fn verifier_key(oracle: &O, _: &O) -> Self::VerifierKey {
