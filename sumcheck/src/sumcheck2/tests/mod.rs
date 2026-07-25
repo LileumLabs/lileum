d33452c6b44286b1a349b8e348e897193fc5bdf6
fabri236 <fabrizio_m@hotmail.com>
2026-07-25 18:41:44 -0300

    Add test for composite and core oracle

--- sumcheck/src/sumcheck2/tests/mod.rs
+++ sumcheck/src/sumcheck2/tests/mod.rs
@@ -3,6 +3,7 @@ use sponge::{poseidon2::PoseidonDefault, sponge::Sponge};
 /// A sponge with rate 2 and capacity 1 over the poseidon2 permutation.
 type Poseidon<F> = Sponge<F, PoseidonDefault<F>, 2, 1, 3>;
 
+mod composite;
 mod sum_of_products;
 mod sumfold;
 mod zerocheck;
