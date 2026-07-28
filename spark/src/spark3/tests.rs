7e0cc6c17bf0422f9a74fd7cfef185c897e98a64
fabri236 <fabrizio_m@hotmail.com>
2026-07-28 18:43:37 -0300

    Add test for spark

--- spark/src/spark3/tests.rs
+++ spark/src/spark3/tests.rs
@@ -4,7 +4,7 @@ use crate::spark3::{
 use ark_ff::PrimeField;
 use commit::commit2::{CommitmentScheme, OpeningRelation};
 use hash_to_curve::svdw::SvdwMap;
-use rand::{rngs::StdRng, SeedableRng};
+use rand::{rngs::StdRng, Rng, SeedableRng};
 use sponge::{poseidon2::PoseidonDefault, sponge::Sponge};
 use std::{iter::repeat, rc::Rc};
 use sumcheck::polynomials::MultiPoint;
@@ -67,4 +67,54 @@ fn single_dimension() {
     // pub type FrScheme = IpaCommitmentScheme<Fr, Projective, SvdwMap<VestaConfig>>;
     type Scheme = IpaCommitmentScheme<Fr, Projective, SvdwMap<VestaConfig>>;
     single_dimension_test::<Fr, Scheme>();
+}
+
+fn two_dimensions_test<F, C>()
+where
+    F: PrimeField,
+    C: CommitmentScheme<F>,
+{
+    let mut rng = StdRng::seed_from_u64(0);
+
+    let addresses: Vec<[u8; 2]> = repeat(()).map(|_| rng.gen()).take(1 << VARS).collect();
+    let values: Vec<F> = repeat(())
+        .map(|_| F::rand(&mut rng))
+        .take(1 << VARS)
+        .collect();
+    let mle = Rc::new(SparseMle::new(addresses, values));
+    let mle = StaticSparkStructure { mle: mle.clone() };
+
+    let pcs = C::new(VARS);
+    let prover = Prover::<F, Poseidon<F>, _, _, SparkReduction<F, C, 2>>::new(&mle, &pcs, VARS);
+    let verifier = Verifier::<F, Poseidon<F>, _, _, SparkReduction<F, C, 2>>::new(&mle, &pcs, VARS);
+
+    let point = [(); VARS].map(|_| F::rand(&mut rng)).to_vec();
+    let point = MultiPoint::new(point);
+
+    let eval = mle.mle.eval(&point);
+
+    let instance = SparkInstance::new(point, eval);
+
+    assert!(StaticSparkRelation::check(&mle, &instance, &()));
+
+    let ProverOutput {
+        instance: open_instance,
+        witness,
+        proof,
+    } = prover.prove(instance.clone(), ());
+
+    assert!(OpeningRelation::check(&pcs, &open_instance, &witness));
+
+    let verifier_instance = verifier.verify(instance, proof).unwrap();
+
+    assert_eq!(open_instance, verifier_instance);
+}
+
+#[test]
+fn two_dimensions() {
+    use ark_vesta::{Fr, Projective, VestaConfig};
+    use commit::ipa2::IpaCommitmentScheme;
+
+    type Scheme = IpaCommitmentScheme<Fr, Projective, SvdwMap<VestaConfig>>;
+    two_dimensions_test::<Fr, Scheme>();
 }
