bff757e7d98c8152b065ec3ee8ed4ed7adf60a92
fabri236 <fabrizio_m@hotmail.com>
2026-07-30 18:36:19 -0300

    Extract module

---
+++ lilium/src/lilium2/reductions/tests/lcs_argument.rs
@@ -0,0 +1,55 @@
+use super::{commit_witness, Poseidon};
+use crate::{
+    lilium2::{
+        reductions::LcsArgument,
+        relations::{LcsRelation, LcsStructure},
+    },
+    testing::utils::HashChain,
+};
+use ark_ff::PrimeField;
+use ccs::{circuit::BuildStructure, structure::CcsStructure};
+use commit::commit2::CommitmentScheme;
+use transcript::reduction2::{Prover, ProverOutput, Relation, Verifier};
+
+fn test<F, C, const N: usize>()
+where
+    F: PrimeField,
+    C: CommitmentScheme<F>,
+{
+    let ccs_structure: CcsStructure<F, 4, 5> =
+        <HashChain<N> as BuildStructure<F, 1, 1, 1, 4>>::structure();
+
+    let pcs = C::new(ccs_structure.vars());
+    let structure = LcsStructure { ccs_structure, pcs };
+
+    let prover: Prover<F, Poseidon<F>, LcsRelation<F, C, 2, 4, 5>, (), LcsArgument> =
+        Prover::new(&structure, &(), ());
+
+    let verifier: Verifier<F, Poseidon<F>, LcsRelation<F, C, 2, 4, 5>, (), LcsArgument> =
+        Verifier::new(&structure, &(), ());
+
+    let input = F::from(8u8);
+    let (instance, witness) = commit_witness::<F, C, N>(&structure.pcs, [input]);
+
+    assert!(LcsRelation::<F, C, 2, 4, 5>::check(
+        &structure, &instance, &witness
+    ));
+    let ProverOutput {
+        instance: _,
+        witness: _,
+        proof,
+    } = prover.prove(instance.clone(), witness);
+
+    verifier.verify(instance, proof).unwrap();
+}
+
+#[test]
+fn lcs_argument() {
+    use ark_vesta::{Fr, Projective, VestaConfig};
+    use commit::ipa2::IpaCommitmentScheme;
+    use hash_to_curve::svdw::SvdwMap;
+
+    type Scheme = IpaCommitmentScheme<Fr, Projective, SvdwMap<VestaConfig>>;
+
+    test::<Fr, Scheme, 3>();
+}
