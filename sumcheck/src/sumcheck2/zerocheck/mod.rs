191a2fe603446040947c15105bac85d33d668cbe
fabri236 <fabrizio_m@hotmail.com>
2026-07-31 21:56:51 -0300

    Expose type

--- sumcheck/src/sumcheck2/zerocheck/mod.rs
+++ sumcheck/src/sumcheck2/zerocheck/mod.rs
@@ -2,7 +2,7 @@ use crate::{sumcheck2::oracles::Oracle, zerocheck::CompactPowers};
 use ark_ff::Field;
 
 pub use reduction::{ZerocheckReduction, ZerocheckSumcheckReduction};
-pub use relation::{ZeroSumcheck, Zerocheck};
+pub use relation::{ZeroSumcheck, Zerocheck, ZerocheckError};
 
 //TODO: delete module
 mod evals;
